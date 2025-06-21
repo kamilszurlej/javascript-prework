221be6dc18c6bb7049de9f9c300681c56867096b
Kamil Szurlej <kamilszurlej123@gmail.com>
2025-06-21 14:26:46 +0200

    Update html and js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var computerMove, playerMove;
+playerMove = 'papier';
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '. Twój ruch to ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
